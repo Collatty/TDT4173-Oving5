e7d9e96a036205ac015eb6cfc2f1811a1f042e29
Mathias Dehli Klev <mathiasklev@dhcp-10-22-60-62.wlan.ntnu.no>
2019-11-15 11:10:44 +0100

    improved detection for test image 2 and refactored code

--- character_detection.py
+++ character_detection.py
@@ -9,117 +9,121 @@ import string
 
 classifier = load_model()
 
-def preprocess(image):
-       #flatten array
-       image_arr = image.flatten()
-       #PCA
-       pca = load_pca()
-       image_arr = pca.transform([image_arr])
-       return image_arr
 
-def classify(image, threshold=0.8):
-       image = preprocess(image)
-       predicted = classifier.predict_proba(image)
-       pred_index = predicted.argmax()
-       pred_val = predicted[0][pred_index]
-       if(pred_val >= threshold):
-              return (pred_index, pred_val)
-       else:
-              return -1
+def preprocess(image):
+    # flatten array
+    image_arr = image.flatten()
+    # PCA
+    pca = load_pca()
+    image_arr = pca.transform([image_arr])
+    return image_arr
+
+
+def classify(image, threshold=0.68):
+    image = preprocess(image)
+    predicted = classifier.predict_proba(image)
+    pred_index = predicted.argmax()
+    pred_val = predicted[0][pred_index]
+    if pred_val >= threshold:
+        return pred_index, pred_val
+    else:
+        return -1
+
 
 def load_image():
-       image = cv.imread('./detection-images/detection-1.jpg', 0)
-       #image = cv.imread('./detection-images/detection-2.jpg', 0)
-       pixels = np.divide(image, 255)
-       return pixels
+    image = cv.imread('./detection-images/detection-2.jpg', 0)
+    # image = cv.imread('./detection-images/detection-1.jpg', 0)
+    pixels = np.divide(image, 255)
+    return pixels
 
-def sliding_window(image, stepSize, windowSize):
-       # slide a window across the image
-       for y in range(0, image.shape[0] - windowSize[0], 2*stepSize):
-              for x in range(0, image.shape[1] - windowSize[1], stepSize):
-                     # yield the current window
-                     yield (y, x, image[y:(y + windowSize[0]), x:(x + windowSize[1])])
 
-image = load_image()
-temp = image
-windowSize = (20,20)
-predicted = {}
-for (y,x, window) in sliding_window(image, stepSize=2, windowSize=windowSize):
-       pred = classify(window)
-       if(pred!=-1):
-              #print('(x,y)=({},{})'.format(x,y),pred)
-              k = pred[0]
-              if k in predicted:
-                     predicted[k].append([(y,x),pred,window])
-              else:
-                     predicted[k] = [[(y,x),pred,window]]
-              #plt.imshow(window,cmap="gray")
-              #plt.show()
+def sliding_window(image, step_size, window_size):
+    # slide a window across the image
+    for y in range(0, image.shape[0] - window_size[0], step_size):
+        for x in range(0, image.shape[1] - window_size[1], step_size):
+            # yield the current window
+            yield (y, x, image[y:(y + window_size[0]), x:(x + window_size[1])])
 
-## if two or more windows are close together (e.g. euclidean distance of 5), then chose the one with
-## higher predicted probability
 
-windows = {}
-for k,v in predicted.items():
-       windows[k] = []
-       if(len(v)==1):
-              windows[k].append(v[0])
-       else:
-              #print(v)
-              coordinates = []
-              pred = []
-              for l in v:
-                     coordinates.append(l[0])
-                     pred.append(l[1][1])
+def classify_best_windows(image, step_size=1, window_size=(20, 20)):
+    predicted = {}
+    c = 0
+    for (y, x, window) in sliding_window(image, step_size, window_size):
+        pred = classify(window)
+        if pred != -1:
+            predicted[c] = [[(y, x), pred, window]]
+            c += 1
+    return predicted
 
-              euclidean = euclidean_distances(coordinates)
-              neighbour_list = []
-              for row in euclidean:
-                     neighbours =[]
-                     for i in range(len(row)):
-                            # 5 is the distance threshold
-                            if(row[i] == 0) or (row[i] < 10):
-                                   neighbours.append(i)
-                     neighbour_list.append(neighbours)
 
-              neighbour_list.sort()
-              neighbour_list = list(neighbour_list for neighbour_list, _ in itertools.groupby(neighbour_list))
+def separate_data_in_lists(predicted):
+    coordinates = []
+    pred = []
+    prob = []
+    for k, v in predicted.items():
+        for l in v:
+            coordinates.append(l[0])
+            pred.append(l[1][0])
+            prob.append(l[1][1])
+    return coordinates, pred, prob
 
-              #indeces of windows with highest predicted value of nearby windows
-              window_indices = []
-              for group in neighbour_list:
-                     max_i = 0
-                     max_pred = 0
-                     for i in group:
-                            pred_prob = v[i][1][1]
-                            if pred_prob > max_pred:
-                                   max_pred = pred_prob
-                                   max_i = i
 
-                     window_indices.append(max_i)
+def make_neighbour_list(coordinates, pred, prob):
+    euclidean = euclidean_distances(coordinates)
+    neighbour_list = []
+    for row in euclidean:
+        neighbours = []
+        for i in range(len(row)):
+            # 20 is the distance threshold
+            if row[i] < 20:
+                neighbours.append([coordinates[i], pred[i], prob[i]])
+        neighbour_list.append(neighbours)
+    return neighbour_list
 
-              for i in window_indices:
-                     windows[k].append(v[i])
 
-       #print(windows)
+# help function return the third element of a list
+def sort_third(val):
+    return val[2]
 
-for k,v in windows.items():
-       for w in v:
-              print(w[:-1])
-              plt.imshow(w[-1],cmap="gray")
-              plt.show()
+# if two or more windows are close together (e.g. euclidean distance of 20), then chose the one with
+# higher predicted probability
 
-# draw window on image
 
-temp = cv.imread('./detection-images/detection-1.jpg',cv.IMREAD_COLOR)
-for k,v in windows.items():
-       for w in v:
-              y,x = w[0]
-              cv.rectangle(temp, (x, y), (x + windowSize[1], y + windowSize[0]), (0, 255, 0), 1) # draw rectangle on image
-              letter = string.ascii_lowercase[w[1][0]]
-              print(letter)
-              cv.putText(temp, letter, (x,y + 2*windowSize[0]), cv.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 0), 1)
-              plt.imshow(temp,cmap="gray", interpolation='nearest')
-              # show all windows
-plt.show()
+def remove_clusters(neighbour_list):
+    final_windows = []
+    for i in range(0, len(neighbour_list)):
+        neighbour_list[i].sort(key=sort_third, reverse=True)
+        final_windows.append(neighbour_list[i][0][0:2])
+    return final_windows
+
+
+def remove_duplicates(final_windows):
+    final_windows.sort()
+    final_windows = list(final_windows for final_windows, _ in itertools.groupby(final_windows))
+    return final_windows
+
+
+def main():
+    image = load_image()
+    window_size = (20, 20)
+    temp = cv.imread('./detection-images/detection-2.jpg', cv.IMREAD_COLOR)
+    # temp = cv.imread('./detection-images/detection-1.jpg', cv.IMREAD_COLOR)
+    predicted = classify_best_windows(image, step_size=1, window_size=(20, 20))
+    (coordinates, pred, prob) = separate_data_in_lists(predicted)
+    neighbour_list = make_neighbour_list(coordinates, pred, prob)
+    final_windows = remove_clusters(neighbour_list)
+    final_windows = remove_duplicates(final_windows)
+
+    for k in final_windows:
+        y, x = k[0]
+        cv.rectangle(temp, (x, y), (x + window_size[1], y + window_size[0]), (0, 255, 0), 1)  # draw rectangle on image
+        letter = string.ascii_lowercase[k[1]]
+        print(letter)
+        cv.putText(temp, letter, (x, y + 2 * window_size[0]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
+        plt.imshow(temp, cmap="gray", interpolation='nearest')
+
+    plt.show()
+
+
+main()
 
